2987b4250b51b7266b1342c880b3e236faaaf14d
Dmitry Kruglov <kruglov081289@mail.ru>
2020-09-09 22:34:42 +0300

    add bootstrap

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+import 'bootstrap';
+
 import _ from 'lodash';
 
 export default () => console.log(_.toUpper('hello world'));
--- webpack.config.js
+++ webpack.config.js
@@ -11,4 +11,21 @@ module.exports = {
         template: 'index.html',
       }),
   ],
+  module: {
+    rules: [
+      { test: /\.css$/, use: ['style-loader', 'css-loader', 'postcss-loader'] },
+      {
+        test: /\.scss$/,
+        use: ['style-loader', 'css-loader', 'postcss-loader', 'sass-loader'],
+      },
+      // {
+      //   test: /\.woff2?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+      //   use: 'url-loader?limit=10000',
+      // },
+      // {
+      //   test: /\.(ttf|eot|svg)(\?[\s\S]+)?$/,
+      //   use: 'file-loader',
+      // },
+    ],
+  },
 };
